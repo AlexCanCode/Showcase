2decfa582ecd13714097b3bec23f097a20da4597
AlexCanCode <AlexCanCode18@gmail.com>
2018-11-27 16:31:28 -0700

    make list of todo items

---
+++ main.js
@@ -0,0 +1,22 @@
+/*list of things needed:
+- nav back to top button that follows page
+- make grid and icons for portfolio
+- choose color pallete
+	- implement for headers and other things
+- consider what font should be used (keep it basic, no more than two on the page)
+- figure out d3 chart
+	- link daily events to appear in tooltip when you hover over day
+	- center chart
+	- determine better name for chart
+- light/dark theme button and styles (probably around the end and may require some refactoring of app)
+	- also need to align properly (all the way to far right (have it duck under the menu on mobile))
+- design the actual project card to look nice
+- take pictures of projects and write descriptions
+- footer
+- get proper picture 
+- consider if you really want button animation (perhaps make shorter)
+- transition all styles over to the css sheet and clean up as needed 
+
+
+
+*/
