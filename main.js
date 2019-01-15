5b363a33ad660ad2382466e9d550f0314911b866
AlexCanCode <AlexCanCode18@gmail.com>
2019-01-15 11:26:29 -0700

    make navbar condense on scrill and stay fixed

--- main.js
+++ main.js
@@ -1,31 +1,9 @@
-/*list of things needed:
-- nav back to top button that follows page
-- make grid and icons for portfolio
-- choose color pallete
-	- implement for headers and other things
-- consider what font should be used (keep it basic, no more than two on the page)
-- figure out d3 chart
-	- link daily events to appear in tooltip when you hover over day
-	- center chart
-	- determine better name for chart
-- light/dark theme button and styles (probably around the end and may require some refactoring of app)
-	- also need to align properly (all the way to far right (have it duck under the menu on mobile))
-- design the actual project card to look nice
-- take pictures of projects and write descriptions
-- footer
-- get proper picture 
-- consider if you really want button animation (perhaps make shorter)
-- transition all styles over to the css sheet and clean up as needed 
-- consider adding some kind of transition in animaiton or "thing"
-- Strike through animation on top links (change their color from that they are now too)
-- Perhaps, go with bright colored stripe (very thin) on top and then a same colored header - for consideration
-- have icons to things like facebook, linked in, and other things in footer? 
-- Link chevron should be smooth action and not just an abrupt arrival at the bookmark
-- Bad Pun Generator
-
---- Useful Sites for Inspiration ---
- - Cool project cards - https://www.harveydelaney.com/
- - 
-
-
-*/
+$(window).scroll(function() {
+	console.log($(document).scrollTop())
+	if ($(document).scrollTop() > 50) {
+		$("nav").addClass("shrink");
+	}
+	else {
+		$("nav").removeClass("shrink");
+	}
+});
